a55f505f2e934cf61cf985249527557fdd07ce90
ElfNevisokogoRosta <anatoliy.tymoshenko.work@gmail.com>
2023-07-05 18:51:47 +0300

    hw-03

--- server.ts
+++ server.ts
@@ -1,4 +1,15 @@
 import app from "./app";
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port 3000");
-});
+import mongoose from "mongoose";
+const DB_HOST =
+  "mongodb+srv://Kolya:123456789A@cluster0.vjpjfz5.mongodb.net/contacts_book?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port 3000");
+    })
+  )
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
